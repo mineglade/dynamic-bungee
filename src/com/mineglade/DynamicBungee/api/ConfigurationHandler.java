e801f3ac8e0b6f98dc96d75f0fd31039559e4696
Mia <mia@mia.cx>
2021-12-19 10:35:37 +0100

    lots of API stuff
    added ConfigurationHandler, Server (with loads of deprecated bullshit) and a ServerBuilder

---
+++ src/com/mineglade/DynamicBungee/api/ConfigurationHandler.java
@@ -0,0 +1,109 @@
+package com.mineglade.DynamicBungee.api;
+
+import com.google.common.io.ByteStreams;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+import java.io.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+
+public class ConfigurationHandler {
+
+    private File path;
+    private File file;
+
+    private Configuration configuration;
+
+    private Plugin plugin;
+
+    public ConfigurationHandler(Plugin plugin) throws IOException {
+        this.plugin = plugin;
+
+        loadConfig();
+        mapServers(getServers());
+    }
+
+    public Configuration getConfig() {
+        return configuration;
+    }
+
+    public ConfigurationHandler loadConfig() throws IOException {
+
+        this.path = this.plugin.getDataFolder();
+
+        if (!path.exists()) {
+            if (!path.mkdir()) {
+                this.plugin.getLogger().log(Level.WARNING, "There was an error creating the plugin's data folder.");
+                throw new IOException("Unable to create plugin's data folder.");
+            }
+            this.plugin.getLogger().log(Level.INFO, "Created plugin's data folder.");
+        }
+
+        this.file = new File(path, "config.yml");
+
+        if (!file.exists()) {
+            if (!file.createNewFile()) {
+                this.plugin.getLogger().log(Level.WARNING, "There was an error while creating the configuration file.");
+                throw new IOException("Unable to create the configuration file.");
+            }
+            this.plugin.getLogger().log(Level.INFO, "Created configuration file.");
+        }
+
+        this.configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
+        return this;
+    }
+
+    public ConfigurationHandler saveConfig() {
+        try {
+            ConfigurationProvider.getProvider(YamlConfiguration.class).save(getConfig(), file);
+        } catch (IOException e) {
+            ProxyServer.getInstance().getLogger().log(Level.SEVERE, "Could not save configuration file, some dynamic servers may not have been saved to disk, and will not appear on server restart.");
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+    // TODO
+    public ConfigurationHandler addServer(Server server) {
+        Configuration serverSection = getConfig().getSection("servers");
+        serverSection.set(server+".host", server.getHost());
+        serverSection.set(server+".motd", server.getMotd());
+        serverSection.set(server+".restricted", server.getRestricted());
+        return this;
+    }
+
+    public ConfigurationHandler removeServer(Server server) {
+        getConfig().set("servers." + server.getName(), null);
+        return null;
+    }
+
+    public Collection<String> getServers() {
+        return getConfig().getSection("servers").getKeys();
+    }
+
+    public Map<String, Server> mapServers(Collection<String> names) {
+        Map<String, Server> servers = new HashMap<>();
+        names.forEach(name -> {
+            Configuration serverSection = getConfig().getSection("servers." + name);
+            System.out.println(name);
+            System.out.println(serverSection.getString("host"));
+            System.out.println(serverSection.getString("motd"));
+            try {
+                new ServerBuilder(name)
+                        .withHost(serverSection.getString("host"))
+                        .withMotd(serverSection.getString("motd"))
+                        .withRestricted(serverSection.getBoolean("restricted"))
+                        .create();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            }
+        });
+        return servers;
+    }
+}
