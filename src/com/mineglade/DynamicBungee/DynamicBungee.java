5553f40b62410e4f417e10d53d1e777f87de8a17
Mia <mia@mia.cx>
2022-07-24 02:50:30 +0200

    re-add bstats

--- src/com/mineglade/DynamicBungee/DynamicBungee.java
+++ src/com/mineglade/DynamicBungee/DynamicBungee.java
@@ -1,60 +1,71 @@
 package com.mineglade.DynamicBungee;
 
 import com.mineglade.DynamicBungee.api.ConfigurationHandler;
+import java.io.IOException;
+import java.util.concurrent.Callable;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 import org.bstats.bungeecord.Metrics;
 
-import java.io.IOException;
-import java.util.concurrent.Callable;
-
 public class DynamicBungee extends Plugin {
 
-    private static DynamicBungee instance;
+  private static DynamicBungee instance;
 
-    private ConfigurationHandler configuration;
+  private ConfigurationHandler configuration;
 
-    public DynamicBungee() {
-        instance = this;
+  public DynamicBungee() {
+    instance = this;
+  }
+
+  @Override
+  public void onEnable() {
+    try {
+      configuration = new ConfigurationHandler(this);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+    getConfig().mapServers(getConfig().getServers());
+    ProxyServer
+      .getInstance()
+      .getPluginManager()
+      .registerCommand(this, new DynamicBungeeCommand(this));
+    
+    int pluginId = 9945; // <-- Replace with the id of your plugin!
+    metrics = new Metrics(this, pluginId);
+  }
 
+  @Override
+  public void onDisable() {
+    ProxyServer.getInstance().getPluginManager().unregisterCommands(this);
 
-    @Override
-    public void onEnable() {
-        try {
-            configuration = new ConfigurationHandler(this);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        getConfig().mapServers(getConfig().getServers());
-        ProxyServer.getInstance().getPluginManager().registerCommand(this, new DynamicBungeeCommand(this));
+    metrics.addCustomChart(new Metrics.SingleLineChart("connected_servers", new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return 0;
+            }
+        }));
     }
+  }
 
-    @Override
-    public void onDisable() {
-        ProxyServer.getInstance().getPluginManager().unregisterCommands(this);
+  //    public static ArrayList<ServerInfo> listServers(DynamicBungee plugin) {
+  //
+  //        ArrayList<ServerInfo> serverInfos = new ArrayList<>(ProxyServer.getInstance().getServers().values());
+  //
+  //        plugin.metrics.addCustomChart(new Metrics.SingleLineChart("connected_servers", new Callable<Integer>() {
+  //
+  //            @Override
+  //            public Integer call() throws Exception {
+  //                return serverInfos.size();
+  //            }
+  //        }));
+  //        return serverInfos;
+  //    }
 
-    }
+  public static DynamicBungee getInstance() {
+    return instance;
+  }
 
-//    public static ArrayList<ServerInfo> listServers(DynamicBungee plugin) {
-//
-//        ArrayList<ServerInfo> serverInfos = new ArrayList<>(ProxyServer.getInstance().getServers().values());
-//
-//        plugin.metrics.addCustomChart(new Metrics.SingleLineChart("connected_servers", new Callable<Integer>() {
-//
-//            @Override
-//            public Integer call() throws Exception {
-//                return serverInfos.size();
-//            }
-//        }));
-//        return serverInfos;
-//    }
-
-    public static DynamicBungee getInstance() {
-        return instance;
-    }
-
-    public ConfigurationHandler getConfig() {
-        return this.configuration;
-    }
+  public ConfigurationHandler getConfig() {
+    return this.configuration;
+  }
 }
